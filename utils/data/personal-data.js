3bd213f60053995ccb64d9e9da8f33a3d53d0ba2
haridas-official <haridas.p@ezeeinfo.in>
2025-11-28 20:47:39 +0530

    first commit

---
+++ utils/data/personal-data.js
@@ -0,0 +1,17 @@
+export const personalData = {
+  name: "HARIDAS P",
+  profile: '/profile.jpg',
+  designation: "Software Developer",
+  email: 'haridasnamakkal@gmail.com',
+  phone: '+91 8508746641',
+  address: 'Namakkal, Tamil Nadu, India',
+  github: 'https://github.com/haridas4274',
+  facebook: 'https://www.facebook.com/haridas4274/',
+  linkedIn: 'https://www.linkedin.com/in/haridas4274/',
+  twitter: 'https://twitter.com/haridas42674',
+  stackOverflow: 'https://stackoverflow.com/users/20822254/haridas-developer',
+  leetcode: "https://leetcode.com/haridas4274/",
+  devUsername: "haridas4274",
+  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
+}
+personalData.description = `My name is ${personalData.name}. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.`;
