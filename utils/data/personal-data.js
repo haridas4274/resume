eb76e81bde83543d094af4d532f674d22b9d2a9f
haridas-official <haridas.p@ezeeinfo.in>
2025-11-29 08:48:58 +0530

    feat: update resume URL

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -12,6 +12,6 @@ export const personalData = {
   stackOverflow: 'https://stackoverflow.com/users/20822254/haridas-developer',
   leetcode: "https://leetcode.com/haridas4274/",
   devUsername: "haridas4274",
-  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
+  resume: "https://github.com/haridas4274/haridas/raw/refs/heads/main/resume/Haridas_resume.pdf"
 }
 personalData.description = `My name is ${personalData.name}. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.`;
